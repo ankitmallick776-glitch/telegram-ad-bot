858ab6d8f396865312972042470bf2f1cdd9a78b
ankitmallick776-glitch <ankitmallick776@gmail.com>
2025-12-04 19:59:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import logging
 import os
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
-from handlers.watch_ads_handler import start, watch_ads, test_reward, balance, get_main_keyboard
+from handlers.watch_ads_handler import start, watch_ads, web_app_data, balance, get_main_keyboard
 
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
@@ -23,13 +22,13 @@ async def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.Regex("^(📺 Watch Ads 💰)$"), watch_ads))
     app.add_handler(MessageHandler(filters.Regex("^(Balance 💳)$"), balance))
-    app.add_handler(MessageHandler(filters.Regex("^(TEST REWARD 💰)$"), test_reward))
+    app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
     
     async def unknown(update, context):
         await update.message.reply_text("👇 Use buttons!", reply_markup=get_main_keyboard())
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unknown))
     
-    print("🤖 Bot started!")
+    print("🤖 Bot + WebApp ready!")
     await app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
